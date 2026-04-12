530259d42cabbb3a703fc89c3111a0ddfb63a371
Sameerali0 <sameer.ali.professional@gmail.com>
2026-04-12 14:54:21 +0500

    added word counter with simple code

---
+++ word_counter.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+#define lINE_LENGTH 1024
+
+int main() {
+    FILE *filePointer = fopen("repeated.txt", "r");
+
+    char buffer[lINE_LENGTH];
+    int lines = 0, words = 0;
+    char lastChar = ' ';
+
+    return 0;
+}
