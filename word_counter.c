0a730f980f6f393ecad05abf520cac5ffe5186bc
Sameerali0 <sameer.ali.professional@gmail.com>
2026-04-13 01:21:16 +0500

    added word and line counter in C

--- word_counter.c
+++ word_counter.c
@@ -1,14 +1,42 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-#define lINE_LENGTH 1024
+#define LINE_LENGTH 1024
 
 int main() {
     FILE *filePointer = fopen("repeated.txt", "r");
 
-    char buffer[lINE_LENGTH];
+    char buffer[LINE_LENGTH];
     int lines = 0, words = 0;
     char lastChar = ' ';
+
+    while (fgets(buffer, sizeof(buffer), filePointer) != NULL)
+    {
+        lines++;
+
+        for (size_t i = 0; buffer[i] != '\0'; i++)
+        {
+            const char current = buffer[i];
+            
+            if ((current == ' ' || current == '\n' || current == '\t') &&
+                (lastChar != ' ' && lastChar != '\n' && lastChar != '\t'))
+            {
+                words++;
+            }
+
+            lastChar = current;
+        }
+        
+    }
+
+    if (lastChar != ' ' && lastChar != '\n' && lastChar != '\t')
+    {
+        words++;
+    }
+
+
+    printf("Lines: %d\n", lines);
+    printf("Words: %d\n", words);
 
     return 0;
 }
